7191dd9555e2c13cf11a4f237c0188d8d0d1417f
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-11-16 17:06:58 +0200

    new

---
+++ 3.1School/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _3._1School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            School school = new School();
+
+            school.name = Console.ReadLine();
+            school.adress = Console.ReadLine();
+            school.phoneNumber = Console.ReadLine();
+            school.website = Console.ReadLine();
+
+            Console.WriteLine("1.School name: ");
+            Console.WriteLine("2.School adress: ");
+            Console.WriteLine("3.School phone number: ");
+            Console.WriteLine("4.School website:  ");
+            Console.WriteLine("5.I ran out of questions.");
+
+            var number = int.Parse(Console.ReadLine());
+            while (number != 5)
+            {
+                switch (number)
+                {
+                    case 1: Console.WriteLine(school.name); break;
+                    case 2: Console.WriteLine(school.adress); break;
+                    case 3: Console.WriteLine(school.phoneNumber); break;
+                    case 4: Console.WriteLine(school.website); break;
+                    case 5: break;
+                    default:
+                        break;
+                }
+                number = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
---
+++ 3.1School/School.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._1School
+{
+    class School
+    {
+        public string name;
+        public string adress;
+        public string phoneNumber;
+        public string website;
+
+    }
+}
