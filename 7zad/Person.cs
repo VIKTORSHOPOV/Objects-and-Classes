76eec837b738496ac3cd7b61354e8c97240cd8d0
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-11-17 17:51:28 +0200

    new

---
+++ 7zad/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7zad
+{
+    class Person
+    {
+        private char sex;
+        private int age;
+        private string name;
+        public char Sex
+        {
+            set
+            {
+                if (value == 'm' || value == 'f' || value == 'M' || value == 'F')
+                {
+                    this.sex = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid data!");
+                }
+            }
+            get { return this.sex; }
+        }
+        public int Age
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    this.age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid data!");
+                }
+            }
+            get { return this.age; }
+        }
+        public string Name
+        {
+            set
+            {
+                if (value.Length > 0)
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid data!");
+                }
+            }
+            get { return this.name; }
+        }
+    }
+}
---
+++ 7zad/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _7zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Console.WriteLine("Sex 1:");
+            person1.Sex = char.Parse(Console.ReadLine());
+            Console.WriteLine("Age 1:");
+            person1.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Name 1:");
+            person1.Name = Console.ReadLine();
+
+            Person person2 = new Person();
+            Console.WriteLine("Sex 2:");
+            person2.Sex = char.Parse(Console.ReadLine());
+            Console.WriteLine("Age 2:");
+            person2.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Name 2:");
+            person2.Name = Console.ReadLine();
+
+            Console.WriteLine(person1.Age > person2.Age ?
+                $"{person1.Name}, {person1.Age}, {person1.Sex}" :
+                $"{person2.Name}, {person2.Age}, {person2.Sex}");
+
+
+        }
+    }
+}
