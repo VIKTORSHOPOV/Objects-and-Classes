f48877fb9a92640aeb635511cbbbcd3b16970383
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-11-17 17:18:07 +0200

    new

--- 6zad/Program.cs
+++ 6zad/Program.cs
@@ -7,14 +7,23 @@ namespace _6zad
         static void Main(string[] args)
         {
             Student student = new Student();
+            Console.WriteLine("Name:");
             student.Name = Console.ReadLine();
+            Console.WriteLine("Class:");
             student.Clas = Console.ReadLine();
+            Console.WriteLine("ID:");
             student.Id = Console.ReadLine();
+            Console.WriteLine("Bel:");
             student.DBel = double.Parse(Console.ReadLine());
+            Console.WriteLine("Foreign:");
             student.DForeign = double.Parse(Console.ReadLine());
+            Console.WriteLine("Math:");
             student.DMath = double.Parse(Console.ReadLine());
+            Console.WriteLine("Physics:");
             student.DPhys = double.Parse(Console.ReadLine());
+            Console.WriteLine("Chemistry:");
             student.DChem = double.Parse(Console.ReadLine());
+            Console.WriteLine("Biology:");
             student.DBio = double.Parse(Console.ReadLine());
             student.Average = (student.DBel + student.DMath + student.DPhys + student.DChem + student.DBio + student.DForeign) / 6;
             Console.WriteLine("Name " + student.Name);
