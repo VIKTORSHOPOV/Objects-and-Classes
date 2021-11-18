046668bb3cc6fccd4b21b1b32733fffe57f4a8a2
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-11-18 15:38:13 +0200

    update

--- 9zad/Program.cs
+++ 9zad/Program.cs
@@ -6,7 +6,16 @@ namespace _9zad
     {
         static void Main(string[] args)
         {
-            
+            Car car = new Car();
+            car.Brand = "Toyota";
+            car.Model = "Yaris";
+            car.Year = 1999;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 5;
+            Console.WriteLine(car.WhoAmI());
+            car.Drive(200);
+            Console.WriteLine(car.WhoAmI());
+
         }
     }
 }
