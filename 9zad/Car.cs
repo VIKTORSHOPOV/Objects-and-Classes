b0df1ad433dfbcc64fd9ade23714b93910fcc3f4
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-11-18 15:31:24 +0200

    new

---
+++ 9zad/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9zad
+{
+    class Car
+    {
+        private string brand;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        public string Brand { get { return brand; } set { brand = value; } }
+        public string Model { get { return model; } set { model = value; } }
+        public int Year { set { if (value >= 1886 && value <= 2021) year = value; else Console.WriteLine("Nekorektni danni"); } get { return year; } }
+        public double FuelQuantity
+        {
+            set { if (value >= 0) fuelQuantity = value; else Console.WriteLine("Nekorektni danni"); }
+            get { return fuelQuantity; }
+        }
+
+        public double FuelConsumption
+        {
+            set { if (value > 0) fuelConsumption = value; else Console.WriteLine("Nekorektni danni"); }
+            get { return fuelConsumption; }
+
+        }
+        public void Drive(double distance)
+        {
+            if (fuelQuantity > distance * fuelConsumption)
+            {
+                fuelQuantity -= distance * fuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+    }
+}
---
+++ 9zad/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
