b8e53e16de2c6e3774dee43c31dd14db4099b659
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-11-22 17:06:08 +0200

    new

---
+++ 10zad/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time time1 = new Time();
+            time1.GetHour();
+            int val1 = int.Parse(Console.ReadLine());
+            int val2 = int.Parse(Console.ReadLine());
+            int val3 = int.Parse(Console.ReadLine());
+            Time time2 = new Time(val1, val2, val3);
+            time2.GetHour();
+        }
+    }
+}
---
+++ 10zad/Time.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10zad
+{
+    class Time
+    {
+        int hours;
+        int minutes;
+        int seconds;
+
+        public Time()
+        {
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        }
+        public Time(int h, int m, int s)
+        {
+            hours = h;
+            minutes = m;
+            seconds = s;
+        }
+
+        public void GetHour()
+        {
+            Console.WriteLine($"{hours:D2}:{minutes:D2}:{seconds:D2}");
+        }
+
+    }
+}
