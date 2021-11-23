013265a8e24bed12a228bd30f2ff69a6710c8e16
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-11-23 17:22:10 +0200

    newa

---
+++ 13.2 zad/BankAccount.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._2_zad
+{
+    class BankAccount
+    {
+        private string accountNumber;
+        private string ownerName;
+        private decimal accountBalance;
+        public string AccountNumber
+        {
+            set { accountNumber = value; }
+            get { return accountNumber; }
+        }
+        public string OwnerName
+        {
+            set { ownerName = value; }
+            get { return ownerName; }
+        }
+        public decimal AccountBalance
+        {
+            set { accountBalance = value; }
+            get { return accountBalance; }
+        }
+        public void MakeDeposit(decimal n)
+        {
+            AccountBalance += n;
+            Console.WriteLine($"Account balance: {AccountBalance}");
+        }
+        public void MakeWithdrawal(decimal sum)
+        {
+            if (accountBalance >= sum)
+            {
+                accountBalance -= sum;
+                Console.WriteLine($"Withdrawn funds: {sum}. Funds available on the account: {accountBalance}");
+            }
+            else
+            {
+                Console.WriteLine($"Non-Sufficient Funds");
+            }
+        }
+        public BankAccount()
+        {
+            accountNumber = "";
+            ownerName = "";
+            accountBalance = 0;
+        }
+        public BankAccount(string number, string name, decimal balance)
+        {
+            accountNumber = number;
+            ownerName = name;
+            accountBalance = balance;
+        }
+    }
+}
---
+++ 13.2 zad/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _13._2_zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            string number = arr[0];
+            string name = arr[1] + " " + arr[2];
+            decimal balance = decimal.Parse(arr[3]);
+            BankAccount person1 = new BankAccount(number, name, balance);
+            while (true)
+            {
+                string comment = Console.ReadLine();
+                if (comment == "End")
+                {
+                    break;
+                }
+                string[] deposit = Console.ReadLine().Split();
+                string depo = deposit[0];
+                decimal moneydepo = decimal.Parse(deposit[1]);
+                if (depo == "Deposit")
+                {
+                    person1.MakeDeposit(moneydepo);
+                }
+                else
+                {
+                    person1.MakeWithdrawal(moneydepo);
+                }
+            }
+        }
+    }
+}
