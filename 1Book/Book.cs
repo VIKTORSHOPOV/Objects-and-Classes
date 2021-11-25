dffc063510d4e2fffd482aab74bb03e00c94eae0
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-11-25 15:29:50 +0200

    update

--- 1Book/Book.cs
+++ 1Book/Book.cs
@@ -13,24 +13,24 @@ namespace _1Book
         int year;
         int count;
         double price;
-        
+
         public string Title
         {
-            set;get;
+            set; get;
         }
         public string Author
         {
-            set;get;
+            set; get;
         }
         public int Year
         {
-            set;get;
+            set; get;
         }
         public int Count
         {
             set
             {
-                if(value >= 0)
+                if (value >= 0)
                 {
                     count = value;
                 }
@@ -48,5 +48,42 @@ namespace _1Book
             }
             get { return price; }
         }
+
+        public Book()
+        {
+            title = "";
+            author = "";
+            year = 0;
+            count = 0;
+            price = 0;
+        }
+
+        public Book(string title, string author, int year, int count, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.year = year;
+            this.count = count;
+            this.price = price;
+        }
+
+        public Book(Book t)
+        {
+            this.title = t.title;
+            this.author = t.author;
+            this.year = t.year;
+            this.count = t.count;
+            this.price = t.price;
+        }
+
+        public void WriteBook()
+        {
+            Console.WriteLine($"{title}, {year}, {author}, {count}");
+        }
+        public double CalcAll()
+        {
+            return count * price;
+        }
+
     }
 }
--- 1Book/Program.cs
+++ 1Book/Program.cs
@@ -6,7 +6,24 @@ namespace _1Book
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string title = Console.ReadLine();
+            string auhtor = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            int count = int.Parse(Console.ReadLine());
+            double price = double.Parse(Console.ReadLine());
+            Book book1 = new Book(title, auhtor, year, count, price);
+            book1.WriteBook();
+
+            title = Console.ReadLine();
+            auhtor = Console.ReadLine();
+            year = int.Parse(Console.ReadLine());
+            count = int.Parse(Console.ReadLine());
+            price = double.Parse(Console.ReadLine());
+            Book book2 = new Book(title, auhtor, year, count, price);
+            book2.WriteBook();
+
+            Console.WriteLine(book1.Year > book2.Year ? book2.CalcAll() : book1.CalcAll());
+
         }
     }
 }
