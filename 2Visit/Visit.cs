5d466f0f22f82d1c17f664dafce92c96f9269f86
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-11-25 15:51:27 +0200

    new

---
+++ 2Visit/Visit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Visit
+{
+    class Visit
+    {
+        string date;
+        string time;
+        int dayOfTheWeek;
+        int quantity;
+        double stay;
+        string nameOfWeb;
+
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int DayOfTheWeek { get { return dayOfTheWeek; } set { if (value > 0 && value < 8) dayOfTheWeek = value; } }
+        public int Quantity { get; set; }
+
+    }
+}
