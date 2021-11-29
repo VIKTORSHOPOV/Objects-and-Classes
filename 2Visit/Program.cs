44428f7d3fcd2a4ab32b3bd315f43ba2f6a0c366
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-11-29 17:17:39 +0200

    update

--- 2Visit/Program.cs
+++ 2Visit/Program.cs
@@ -6,7 +6,24 @@ namespace _2Visit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string date = Console.ReadLine();
+            string time = Console.ReadLine(); ;
+            int dayOfTheWeek = int.Parse(Console.ReadLine());
+            int quantity = int.Parse(Console.ReadLine());
+            int stay = int.Parse(Console.ReadLine());
+            string nameOfWeb = Console.ReadLine();
+            Visit v1 = new Visit(date, time, dayOfTheWeek, quantity, stay, nameOfWeb);
+            v1.InfoForVisit();
+
+            date = Console.ReadLine();
+            time = Console.ReadLine(); ;
+            dayOfTheWeek = int.Parse(Console.ReadLine());
+            quantity = int.Parse(Console.ReadLine());
+            stay = int.Parse(Console.ReadLine());
+            Visit v2 = new Visit(date, time, dayOfTheWeek, quantity, stay);
+            v2.InfoForVisit();
+
+            Console.WriteLine(v1.CalcAllData(v2));
         }
     }
 }
--- 2Visit/Visit.cs
+++ 2Visit/Visit.cs
@@ -12,14 +12,14 @@ namespace _2Visit
         string time;
         int dayOfTheWeek;
         int quantity;
-        double stay;
+        int stay;
         string nameOfWeb;
 
         public string Date { get; set; }
         public string Time { get; set; }
         public int DayOfTheWeek { get { return dayOfTheWeek; } set { if (value > 0 && value < 8) dayOfTheWeek = value; } }
         public int Quantity { get { return quantity; } set { if (value! < 0) quantity = value; } }
-        public double Stay { get { return stay; } set { if (value! < 0) stay = value; } }
+        public int Stay { get { return stay; } set { if (value! < 0) stay = value; } }
         public string NameOfWeb { get; set; }
 
         public Visit()
@@ -32,7 +32,7 @@ namespace _2Visit
             nameOfWeb = "";
         }
 
-        public Visit(string date, string time, int dayOfTheWeek, int quantity, double stay, string nameOfWeb)
+        public Visit(string date, string time, int dayOfTheWeek, int quantity, int stay, string nameOfWeb)
         {
             this.date = date;
             this.time = time;
@@ -42,5 +42,45 @@ namespace _2Visit
             this.nameOfWeb = nameOfWeb;
         }
 
+        public Visit(string date, string time, int dayOfTheWeek, int quantity, int stay) : this()
+        {
+            this.date = date;
+            this.time = time;
+            this.dayOfTheWeek = dayOfTheWeek;
+            this.quantity = quantity;
+            this.stay = stay;
+        }
+
+        public string CalcTime()
+        {
+            int hours = this.stay / 3600;
+            int minutes = this.stay / 60 % 60;
+            int sec = this.stay % 60;
+            return $"{hours:D2}:{minutes:D2}:{sec:D2}";
+        }
+
+        public void InfoForVisit()
+        {
+            string dayOfTheWeekS = "";
+            switch (this.dayOfTheWeek)
+            {
+                case 1: dayOfTheWeekS = "Monday"; break;
+                case 2: dayOfTheWeekS = "Tuesday"; break;
+                case 3: dayOfTheWeekS = "Wednesday"; break;
+                case 4: dayOfTheWeekS = "Thursday"; break;
+                case 5: dayOfTheWeekS = "Friday"; break;
+                case 6: dayOfTheWeekS = "Saturday"; break;
+                case 7: dayOfTheWeekS = "Sunday"; break;
+
+                default:
+                    break;
+            }
+            Console.WriteLine($"{this.date}/{this.time}, {dayOfTheWeekS}, {this.quantity} B, stay {CalcTime()}, {this.nameOfWeb}");
+        }
+
+        public int CalcAllData(Visit v)
+        {
+            return this.quantity + v.quantity;
+        }
     }
 }
