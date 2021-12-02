a5c6630c767e2960e0f02841679ba22ab787b83a
VIKTORSHOPOV <viktor.shopov13@gmail.com>
2021-12-02 16:00:45 +0200

    new

---
+++ Formula1/Driver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formula1
+{
+    class Driver
+    {
+        public string Name
+        {
+            set; get;
+        }
+        public int Age
+        {
+            set; get;
+        }
+
+        public double TotalTime
+        {
+            set; get;
+        }
+
+        public double Speed
+        {
+            set; get;
+        }
+
+        public Driver()
+        {
+            Name = "";
+            Age = 0;
+            TotalTime = 0;
+            Speed = 0;
+        }
+
+        public Driver(string name, int age, double totaltime, double speed)
+        {
+            Name = name;
+            Age = age;
+            TotalTime = totaltime;
+            Speed = speed;
+        }
+
+        public void ShowInfo()
+        {
+
+            Console.WriteLine($"DriverName: { Name } \nDriverAge: { Age }\n Time: { TotalTime } \nSpeed: { Speed } ");
+        }
+    }
+
+
+
+
+}
---
+++ Formula1/Team.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formula1
+{
+    class Team
+    {
+
+        public string TeamName
+        {
+            get; set;
+        }
+        public Driver Pilot1
+        {
+            get; set;
+        }
+        public Driver Pilot2
+        {
+            get; set;
+        }
+
+        public Team()
+        {
+            TeamName = "";
+            Pilot1 = new Driver();
+            Pilot2 = new Driver();
+        }
+        public Team(string teamName, Driver pilot1, Driver pilot2)
+        {
+            TeamName = teamName;
+            Pilot1 = new Driver();
+            Pilot1.Name = pilot1.Name;
+            Pilot1.Age = pilot1.Age;
+            Pilot1.TotalTime = pilot1.TotalTime;
+            Pilot1.Speed = pilot1.Speed;
+
+            Pilot2 = new Driver();
+            Pilot2.Name = pilot2.Name;
+            Pilot2.Age = pilot2.Age;
+            Pilot2.TotalTime = pilot2.TotalTime;
+            Pilot2.Speed = pilot2.Speed;
+
+        }
+
+        public void ShowInfo1()
+        {
+
+            Console.WriteLine($"Team: { TeamName }");
+            Pilot1.ShowInfo();
+
+
+        }
+        public void ShowInfo2()
+        {
+
+            Console.WriteLine($"Team: { TeamName }");
+            Pilot2.ShowInfo();
+
+
+        }
+
+
+
+
+    }
+}
